da659a2a12b3b8099b7f8af9f87c477ad6f615d6
webvasenkov <webvasenkov@gmail.com>
2021-01-01 17:10:04 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import store from './redux/storeRedux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter, HashRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-
-window.store = store
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
     <BrowserRouter>
         <Provider store={store}>
-            <App/>
+            <App />
         </Provider>
     </BrowserRouter>, document.getElementById('root')
 );
